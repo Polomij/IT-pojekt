afba4a0e8bca67515ad5105befbc11a4c8cd0ad5
404Archi <31891495+Polomij@users.noreply.github.com>
2021-11-03 16:50:54 +0100

    03.11.21 v1.2

--- app.js
+++ app.js
@@ -3,14 +3,34 @@ const express = require('express');
 const app = express();
 const httpServer = http.createServer(app);
 const websocket = require('ws');
-const mysql = require('pg');
-const DATABASE_HOST='';
-const DATABASE_USER='';
-const DATABASE_PASSWORD='';
-const DATABASE_NAME='';
 
-const {Client} = require('pg');
+// get the client
+const mysql = require('mysql2');
 
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test'
+});
+
+// simple query
+connection.query(
+  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
+
+// with placeholder
+connection.query(
+  'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+  ['Page', 45],
+  function(err, results) {
+    console.log(results);
+  }
+);
 
 const wss = new websocket.Server({
   server: httpServer
@@ -25,20 +45,3 @@ wss.on('connection', (_ws, _req) => {
   console.log('Socket connected from '+_req.socket.remoteAddress+'.');
 });
 
-
-const main = async () => {
- 		const client = new Client({
-        user: DATABASE_USER,
-        password: DATABASE_PASSWORD,
-        database: DATABASE_NAME,
-        host: DATABASE_HOST,
-    });
-    await client.connect();
-    try {
-        console.log(await client.query('SELECT 1'));
-    } finally {
-        await client.end();
-    }
-};
-
-main().catch(console.error);
